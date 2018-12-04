71258f597df6e336fa11fadc7fba1881e2cd73e0
BhpAlpha <bhp-project@hotmail.com>
2018-12-04 15:20:10 +0800

    initial commit

---
+++ bhpj/ConvOption.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Bhp.Compiler
+{
+    public class ConvOption
+    {
+        public bool useNep8 = false;//call to callI
+        public static ConvOption Default
+        {
+            get
+            {
+                return new ConvOption();
+            }
+        }
+    }
+}
---
+++ bhpn/ConvOption.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bhp.Compiler
+{
+    public class ConvOption
+    {
+        public bool useNep8 = false;//call to callI
+
+        public bool useSysCallInteropHash = false;
+        public static ConvOption Default
+        {
+            get
+            {
+                return new ConvOption();
+            }
+        }
+    }
+}
